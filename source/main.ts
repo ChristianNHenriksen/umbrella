e7d286d989f2df3c121df0639c43f7d9b71fe849
ChristianNHenriksen <cnhenriksen@gmail.com>
2018-04-06 00:01:59 +0200

    Updated main.ts imports

--- source/main.ts
+++ source/main.ts
@@ -1,5 +1,5 @@
 import { Server } from "./server";
-import { BuildRouter, BuildRule } from "./build";
+import { BuildRouter, BuildRule } from "./build/index";
 import { CommandService } from "../source/command/command.service";
 
 const commandService = new CommandService();
@@ -7,5 +7,5 @@ const buildRouter = BuildRouter.create(commandService, BuildRule);
 
 if (!commandService.directoryExists("builds"))
     commandService.makeDirectory("builds");
-
+    
 Server.create(buildRouter, false).listen(3000, () => console.log(`Listening on port 3000`));
