fd805c8146875f65615bab74d80acccfcee65418
ChristianNHenriksen <cnhenriksen@gmail.com>
2018-04-06 12:27:09 +0200

    The server will accept the port as a start argument

--- source/main.ts
+++ source/main.ts
@@ -3,10 +3,16 @@ import { BuildRouter } from "./build/build.router";
 import { BuildRule } from "./build/build.rule";
 import { CommandService } from "../source/command/command.service";
 
+const parameters = process.argv.slice(2);
+if (parameters.length !== 1)
+    throw new Error("The server only accepts one argument: port");
+
+const port = parameters[0];
+
 const commandService = new CommandService();
 const buildRouter = BuildRouter.create(commandService, BuildRule);
 
 if (!commandService.directoryExists("builds"))
     commandService.makeDirectory("builds");
     
-Server.create(buildRouter, false).listen(3000, () => console.log(`Listening on port 3000`));
+Server.create(buildRouter, false).listen(port, () => console.log(`Listening on port ${port}`));
